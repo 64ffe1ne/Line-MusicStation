aeaf22af37f93ef17f777222d2f394e126393e35
yamichang <42953285+64ffe1ne@users.noreply.github.com>
2019-04-05 02:37:55 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests as r
+
+host = 'https://music-jp-api.line-apps.com/v3/bgm/'
+
+lct = ''
+#x-lctを入力
+
+did = ''
+#x-lm-didを入力
+
+mid = input("please input music id:")
+
+points = host + mid
+
+header = {'x-lct': lct, 'Accept': 'application/jsom', 'x-lm-did': did, 'timeout': 30, 'User-Agent': 'LineMusic/3.9.2  (iPhone; U; iOS 12.1.4; ja-JP;)', 'Accept-Language': 'ja-JP;q=1', 'Accept-Encoding': 'br, gzip, deflate', 'Cookie': ''}
+
+p = r.get(points, headers=header)
+
+if p.status_code == 200:
+    print('ok')
+else:
+    p.raise_for_status()
+    print(p.text)
